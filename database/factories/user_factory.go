40e41bcd468fddf6871548b9c8b9f9bfcf630672
yuan <>
2022-09-12 16:59:03 +0800

    seed命令

--- database/factories/user_factory.go
+++ database/factories/user_factory.go
@@ -17,7 +17,7 @@ func MakeUsers(times int) []user.User {
 			Name:     faker.Username(),
 			Email:    faker.Email(),
 			Phone:    helpers.RandomString(11),
-			Password: "$2a$14$oPzVkIdwJ8KqY0erYAYQxOuAAlbI/sFIsH0C0R4MPc.3JbWWSuaUe",
+			Password: "123456",
 		}
 		objs = append(objs, model)
 	}
--- pkg/seed/seeder.go
+++ pkg/seed/seeder.go
@@ -1,7 +1,12 @@
 //处理数据库填充相关逻辑
 package seed
 
-import "gorm.io/gorm"
+import (
+	"gohub/pkg/console"
+	"gohub/pkg/database"
+
+	"gorm.io/gorm"
+)
 
 //存放所有seeder
 var seeders []Seeder
@@ -30,4 +35,46 @@ func Add(name string, fn SeederFunc) {
 //setrunorder设置按顺序执行的seeder数组
 func SetRunOrder(names []string) {
 	orderedSeederNames = names
+}
+
+//getseeder通过名称获取seeder对象
+func GetSeeder(name string) Seeder {
+	for _, sdr := range seeders {
+		if name == sdr.Name {
+			return sdr
+		}
+	}
+	return Seeder{}
+}
+
+//runall运行所有seeder
+func RunAll() {
+	//先运行ordered
+	executed := make(map[string]string)
+	for _, name := range orderedSeederNames {
+		sdr := GetSeeder(name)
+		if len(sdr.Name) > 0 {
+			console.Warning("Running ordered seeder:" + sdr.Name)
+			sdr.Func(database.DB)
+			executed[name] = name
+		}
+	}
+	//在运行剩下的
+	for _, sdr := range seeders {
+		//过滤已运行
+		if _, ok := executed[sdr.Name]; !ok {
+			console.Warning("running seeder: " + sdr.Name)
+			sdr.Func(database.DB)
+		}
+	}
+}
+
+//运行耽搁seeder
+func RunSeeder(name string) {
+	for _, sdr := range seeders {
+		if name == sdr.Name {
+			sdr.Func(database.DB)
+			break
+		}
+	}
 }
